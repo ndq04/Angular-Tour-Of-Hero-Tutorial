01c877f56cfd256a39a2f45b95fafa866df72c26
NGUYEN DUY QUANG <quangnd@OMINEXT.LOCAL>
2022-04-05 09:00:12 +0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  constructor() {}
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Dr Nice' },
+      { id: 2, name: 'Narco' },
+      { id: 3, name: 'Bombasto' },
+      { id: 4, name: 'Celeritas' },
+      { id: 5, name: 'Magneta' },
+      { id: 6, name: 'RubberMan' },
+      { id: 7, name: 'Dynama' },
+      { id: 8, name: 'Dr IQ' },
+      { id: 9, name: 'Magma' },
+      { id: 10, name: 'Tornado' },
+    ];
+    return { heroes };
+  }
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
+      : 1;
+  }
+}
